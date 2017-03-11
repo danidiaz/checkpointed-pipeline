8e134ad3551f0827a7c270b330cb3a7258d7000d
DDC <diaz_carrete@yahoo.com>
2017-03-11 20:55:31 +0100

    I don't know what I'm doing.

--- lib/Control/Checkpointed.hs
+++ lib/Control/Checkpointed.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Control.Checkpointed where
 
-import Data.Semigroup
+import Data.List.NonEmpty
 import Data.Semigroupoid
 
+data Pipeline t r a i o = Pipeline
+                        {
+                          tag :: NonEmpty t 
+                        , recover :: (NonEmpty t -> r) -> IO (Maybe (a () o)) 
+                        , calculate :: (NonEmpty t -> r) -> a i o
+                        }
+
+preparePipeline :: (NonEmpty t -> r) -> Pipeline t r a () o -> IO (a () o)
+preparePipeline f (Pipeline {tag,recover,calculate}) = 
+  do recovery <- recover f
+     return (case recovery of
+        Just p -> p
+        Nothing -> calculate f)
+    
 
