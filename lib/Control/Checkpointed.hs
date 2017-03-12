ca93b486d0d7b93f438335b432c42198fb23e17f
DDC <diaz_carrete@yahoo.com>
2017-03-12 21:46:50 +0100

    Removed some functors.

--- lib/Control/Checkpointed.hs
+++ lib/Control/Checkpointed.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE NamedFieldPuns #-}
-module Control.Checkpointed where
+module Control.Checkpointed (
+        Pipeline
+    ,   stage
+    ,   preparePipeline
+    ) where
 
 import Prelude hiding ((.),id)
 import Control.Category
@@ -9,33 +13,40 @@ import Data.Profunctor
 import Data.Semigroup
 import Data.Semigroupoid
 
-data Pipeline t r a i o = 
+data Pipeline t r a b c = 
     Pipeline
     {
       tag :: NonEmpty t 
-    , recover :: (NonEmpty t -> r) -> IO (Maybe (a () o)) 
-    , calculate :: (NonEmpty t -> r) -> a i o 
+    , recover :: (NonEmpty t -> r) -> IO (Maybe (a () c)) 
+    , calculate :: (NonEmpty t -> r) -> a b c
     } 
+
+stage :: t 
+      -> (r -> IO (Maybe (a () c)))
+      -> (r -> a b c)
+      -> Pipeline t r a b c
+stage = undefined 
    
-instance Arrow a => Functor (Pipeline t r a i) where
-    fmap f (Pipeline tag' recover' calculate') =
-        Pipeline tag' 
-                 (fmap (fmap (fmap (flip (>>^) f))) recover')
-                 (fmap (flip (>>^) f) calculate')
+-- -- maybe the Functor and Profuctor instances shouldn't really be here...
+-- instance Arrow c => Functor (Pipeline t r c i) where
+--     fmap f (Pipeline tag' recover' calculate') =
+--         Pipeline tag' 
+--                  (fmap (fmap (fmap (flip (>>^) f))) recover')
+--                  (fmap (flip (>>^) f) calculate')
+-- 
+-- instance Arrow c => Profunctor (Pipeline t r c) where
+--     rmap = fmap
+--     lmap f (Pipeline tag' recover' calculate') =
+--         Pipeline tag' recover' (fmap ((^>>) f) calculate')
 
-instance Arrow a => Profunctor (Pipeline t r a) where
-    rmap = fmap
-    lmap f (Pipeline tag' recover' calculate') =
-        Pipeline tag' recover' (fmap ((^>>) f) calculate')
-
-preparePipeline :: (NonEmpty t -> r) -> Pipeline t r a () o -> IO (a () o)
+preparePipeline :: (NonEmpty t -> r) -> Pipeline t r c () o -> IO (c () o)
 preparePipeline f (Pipeline {recover,calculate}) = 
   do recovery <- recover f
      return (case recovery of
         Just p -> p
         Nothing -> calculate f)
     
-instance Category a => Semigroupoid (Pipeline t r a) where
+instance Category c => Semigroupoid (Pipeline t r c) where
     o (Pipeline tag2 recover2 calculate2) (Pipeline tag1 recover1 calculate1) =
         Pipeline 
         {
