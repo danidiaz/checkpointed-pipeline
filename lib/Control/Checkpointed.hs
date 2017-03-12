f065391a8454fc51d9c04b0945d5eb55530b9e40
DDC <diaz_carrete@yahoo.com>
2017-03-12 13:37:29 +0100

    ...

--- lib/Control/Checkpointed.hs
+++ lib/Control/Checkpointed.hs
@@ -2,13 +2,14 @@
 module Control.Checkpointed where
 
 import Data.List.NonEmpty
+import Data.Semigroup
 import Data.Semigroupoid
 
 data Pipeline t r a i o = Pipeline
                         {
                           tag :: NonEmpty t 
                         , recover :: (NonEmpty t -> r) -> IO (Maybe (a () o)) 
-                        , calculate :: (NonEmpty t -> r) -> a i o
+                        , calculate :: (NonEmpty t -> r) -> a i o 
                         }
 
 preparePipeline :: (NonEmpty t -> r) -> Pipeline t r a () o -> IO (a () o)
@@ -18,4 +19,10 @@ preparePipeline f (Pipeline {tag,recover,calculate}) =
         Just p -> p
         Nothing -> calculate f)
     
-
+instance Semigroupoid (Pipeline t r a) where
+    o (Pipeline tag2 recover2 calculate2) (Pipeline tag1 recover1 calcualte1) =
+        Pipeline {
+                    tag = tag1 <> tag2
+                 ,  recover = undefined
+                 ,  calculate = undefined
+                 }
