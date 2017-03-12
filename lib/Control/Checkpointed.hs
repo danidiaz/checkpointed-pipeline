7708bfda71c1eef8a95f9b6f0071df95d0de2c5c
DDC <diaz_carrete@yahoo.com>
2017-03-12 23:40:43 +0100

    ...

--- lib/Control/Checkpointed.hs
+++ lib/Control/Checkpointed.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE Arrows #-}
 module Control.Checkpointed (
         Pipeline
     ,   stage
     ,   prepare
+    ,   unlift
+    ,   Data.Semigroupoid.o
     ) where
 
 import Prelude hiding ((.),id)
 import Control.Category
 import Control.Arrow
 import Data.List.NonEmpty
-import Data.Profunctor
 import Data.Semigroup
 import Data.Semigroupoid
 
@@ -18,15 +20,28 @@ data Pipeline t r a b c =
     {
       tag :: NonEmpty t 
     , recover :: (NonEmpty t -> r) -> IO (Maybe (a () c)) 
-    , calculate :: (NonEmpty t -> r) -> a b c
+    , calculate :: a b c
+    , calculate' :: (NonEmpty t -> r) -> a b c
     } 
 
-stage :: t 
-      -> (r -> IO (Maybe (a () c)))
-      -> (r -> a b c)
+stage :: Arrow a
+      => t -- ^ Tag 
+      -> (r -> IO (Maybe (a () c))) -- ^ Recover action
+      -> (r -> a c ()) -- ^ Save action
+      -> a b c -- ^ Computation to perform
       -> Pipeline t r a b c
-stage = undefined 
-   
+stage atag arecover asaver acalculate = Pipeline
+        {
+            tag = pure atag
+        ,   recover = \f -> arecover (f (pure atag))
+        ,   calculate = acalculate
+        ,   calculate' = \f -> 
+                proc b -> do
+                    c <- acalculate -< b
+                    _ <- asaver (f (pure atag)) -< c
+                    returnA -< c
+        }
+
 -- -- maybe the Functor and Profuctor instances shouldn't really be here...
 -- instance Arrow c => Functor (Pipeline t r c i) where
 --     fmap f (Pipeline tag' recover' calculate') =
@@ -39,15 +54,19 @@ stage = undefined
 --     lmap f (Pipeline tag' recover' calculate') =
 --         Pipeline tag' recover' (fmap ((^>>) f) calculate')
 
-prepare :: (NonEmpty t -> r) -> Pipeline t r c () o -> IO (c () o)
-prepare f (Pipeline {recover,calculate}) = 
-  do recovery <- recover f
-     return (case recovery of
+prepare :: (NonEmpty t -> r) -> Pipeline t r a () c -> IO (a () c)
+prepare f (Pipeline {recover,calculate'}) = 
+  do recovered <- recover f
+     return (case recovered of
         Just p -> p
-        Nothing -> calculate f)
+        Nothing -> calculate' f)
+
+unlift :: Pipeline t r a b c -> a b c
+unlift = calculate
     
 instance Category c => Semigroupoid (Pipeline t r c) where
-    o (Pipeline tag2 recover2 calculate2) (Pipeline tag1 recover1 calculate1) =
+    (Pipeline tag2 recover2 calculate2 calculate2') `o` 
+        (Pipeline tag1 recover1 calculate1 calculate1') =
         Pipeline 
         {
            tag = tag1 <> tag2
@@ -59,10 +78,11 @@ instance Category c => Semigroupoid (Pipeline t r c) where
                      Nothing -> do
                         r1 <- recover1 f
                         case r1 of 
-                           Just a1 -> return $ Just $ calculate2 prepending . a1
+                           Just a1 -> return $ Just $ calculate2' prepending . a1
                            Nothing -> return Nothing
-        ,  calculate = \f -> 
+        ,  calculate = calculate2 . calculate1
+        ,  calculate' = \f -> 
                let prepending s = f (tag1 <> s)
-               in calculate2 prepending . calculate1 f  
+               in calculate2' prepending . calculate1' f  
         }
 
